6c96034c256ef61c57ed8428d59862bdc112c86f
BrindhaAnandhan <brindhaorbindu@gmail.com>
2024-01-21 21:20:21 +0530

    Added password encryptions and created views after post method activated

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    userName = models.OneToOneField(User, on_delete = models.CASCADE)
+    
+    username = models.OneToOneField(User, on_delete = models.CASCADE)
     Address = models.TextField()
     Profile_Pic = models.ImageField()
 
--- app/views.py
+++ app/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from app.forms import *
+from django.http import HttpResponse 
 # Create your views here.
 
 def Register(request):
-    ufo = UserForm()
-    pfo = ProfileForm()
-    data = {'ufo':ufo, 'pfo': pfo}
-    return render(request, 'Register.html', data)
+
+    if request.method == 'POST' and request.FILES:
+        ufd = UserForm(request.POST)
+        pfd = ProfileForm(request.POST, request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+
+            #This is for userform module
+            mufd = ufd.save(commit=False)
+            password = ufd.cleaned_data['password']
+            mufd.set_password(password)
+            mufd.save()
+
+
+            #This is for profile module
+            mpfd = pfd.save(commit=False)
+            mpfd.username = mufd
+            mpfd.save() 
+
+            return HttpResponse('Registration Done')
+        else:
+            return HttpResponse('Not Done')
+    else:
+        ufo = UserForm()
+        pfo = ProfileForm()
+        data = {'ufo':ufo, 'pfo': pfo}
+        return render(request, 'Register.html', data)
